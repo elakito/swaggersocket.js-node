35c0957244c76e1ba32372f3597b564b89531c75
Aki Yoshida <elakito@gmail.com>
2015-06-04 01:15:30 +0200

    simplified the module

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-var fs = require("fs"),
-
-window = require("jsdom").jsdom().createWindow();
-window.eval(fs.readFileSync(__dirname + "/lib/swaggersocket.js", "utf-8"));
-window.atmosphere = require("atmosphere-client");
-
-module.exports = window.swaggersocket;
+module.exports = require('./lib/swaggersocket');
